70bcd6162457c5e7baebbfb8116c0a376a27414b
vaishnavibairy <vaishnavibairy7@gmail.com>
2024-12-21 23:42:26 +0530

    commited

--- bills.py
+++
@@ -1,7 +0,0 @@
-salary=int(input("enter the salary "))
-bill1=int(input("enter the bill1 "))
-bill2=int(input("enter the bill2 "))
-bill3=int(input("enter the bill3 "))
-sum1=bill1+bill2+bill3
-finalmoney=(sum1/salary)*100
-print(finalmoney)
